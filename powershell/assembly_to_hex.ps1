047ea475cfef79bf63967391e5262de4f5a85d3e
Chris Thompson <cjt08c@gmail.com>
2021-04-23 17:01:01 -0400

    Added MSSQL new stored procedure command execution technique

---
+++ powershell/assembly_to_hex.ps1
@@ -0,0 +1,7 @@
+﻿$assemblyFile = "C:\cmdExec.dll"
+$stringBuilder = New-Object -Type System.Text.StringBuilder
+$fileStream = [IO.File]::OpenRead($assemblyFile)
+while (($byte = $fileStream.ReadByte()) -gt -1) {
+$stringBuilder.Append($byte.ToString("X2")) | Out-Null
+}
+$stringBuilder.ToString() -join "" | Out-File C:\cmdExec.txt
